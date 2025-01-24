2dfe67c39de3bc97eab37707f59d74bf5c59d5df
Sthewerson <fernandoo-rocha@hotmail.com>
2025-01-24 16:42:12 -0300

    Add JWT authentication endpoints

--- wallets/urls.py
+++ wallets/urls.py
@@ -1,6 +1,7 @@
 # filepath: /home/fernando/Documentos/desafio/wallets/urls.py
 from django.urls import path
 from .views import CreateUserView, UserDetailView, WalletDetailView, AddBalanceView, CreateTransactionView, ListTransactionsView, TransactionHistoryView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('users/', CreateUserView.as_view(), name='create_user'),
@@ -10,4 +11,6 @@ urlpatterns = [
     path('transactions/', CreateTransactionView.as_view(), name='create_transaction'),
     path('transactions/list/', ListTransactionsView.as_view(), name='list_transactions'),
     path('transactions/history/', TransactionHistoryView.as_view(), name='transaction_history'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
